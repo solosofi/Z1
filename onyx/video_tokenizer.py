bbad11b4cd908ef3a4a80f93ba5350f22e12b092
solosofi <sololevelingsofi@gmail.com>
2025-03-08 20:54:33 +0300

    Update video_tokenizer.py

--- onyx/video_tokenizer.py
+++ onyx/video_tokenizer.py
@@ -1,7 +1,9 @@
 """
 Video tokenizer module.
 
-This module compresses a video (sequence of frames) into a compact set of discrete tokens via a causal model.
+This module compresses a video (sequence of frames) into a compact set of discrete tokens.
+In this update the spatial dimension is aggregated via average pooling so that each frame
+is represented by a single token of dimension Config.VIDEO_TOKEN_DIM.
 """
 
 import torch
@@ -11,7 +13,7 @@ from onyx.config import Config
 class VideoTokenizer(nn.Module):
     def __init__(self, in_channels=3, token_dim=Config.VIDEO_TOKEN_DIM):
         super(VideoTokenizer, self).__init__()
-        # A simple CNN backbone to encode frames into latent tokens
+        # A simple CNN backbone to encode frames into latent tokens.
         self.encoder = nn.Sequential(
             nn.Conv2d(in_channels, 32, kernel_size=3, stride=2, padding=1),  # [B, 32, H/2, W/2]
             nn.ReLU(),
@@ -24,10 +26,8 @@ class VideoTokenizer(nn.Module):
         B, T, C, H, W = video_frames.shape
         video_frames = video_frames.view(B * T, C, H, W)
         tokens = self.encoder(video_frames)  # [B*T, token_dim, H/4, W/4]
-        # Flatten spatial dimensions
-        tokens = tokens.view(B, T, tokens.shape[1], -1)  # [B, T, token_dim, spatial]
-        # Permute to [B, T, spatial, token_dim]
-        tokens = tokens.permute(0, 1, 3, 2)
-        # Collapse spatial tokens
-        tokens = tokens.flatten(2)  # [B, T, total_tokens]
+        # Pool spatial dimensions (average over H/4 x W/4)
+        tokens = tokens.mean(dim=[2, 3])  # [B*T, token_dim]
+        # Reshape back to [B, T, token_dim]
+        tokens = tokens.view(B, T, -1)
         return tokens
